39fa844b7160c2d09b3bf448364a984fb6a17f20
«Viktoriya <v.oberemko@mail.ru>
2022-07-14 11:34:47 +0300

    Add SolutionTask1

---
+++ SolutionTask1/Program.cs
@@ -0,0 +1,25 @@
+﻿string? inputLkneOne = Console.ReadLine();
+string? inputLkneTwo = Console.ReadLine();
+
+if(inputLkneOne != null && inputLkneTwo !=null)
+{
+  int inputNumberOne = int.Parse(inputLkneOne);
+  int inputNumberTwo = int.Parse(inputLkneTwo);
+//  if(inputNumberOne == inputNumberTwo * inputNumberTwo)
+ //       {
+ //           Console.WriteLine("Yes");
+ //       }
+ //      else
+ //       {
+ //           Console.WriteLine("No");
+//        }
+//}
+    if(inputNumberTwo == Math.Sqrt(inputNumberOne))
+        {
+            Console.WriteLine("Yes");
+        }
+       else
+        {
+            Console.WriteLine("No");
+        }
+}        
