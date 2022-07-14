5c93ce2563d29688451a91ea0db452cfd31c2f35
«Viktoriya <v.oberemko@mail.ru>
2022-07-14 11:39:21 +0300

    Add SolutionTask8_HomeWork4

---
+++ SolutionTask8_HW4/Program.cs
@@ -0,0 +1,15 @@
+﻿string? inputLkneOne = Console.ReadLine();
+
+if(inputLkneOne != null)
+{
+   int inputNumber = int.Parse(inputLkneOne);
+   int startNumber =  2;
+   string lineOutput = "";
+   while (startNumber < inputNumber)
+   {
+    lineOutput = lineOutput + startNumber +", ";
+    startNumber = startNumber + 2;
+   }
+    
+   Console.WriteLine(lineOutput);
+}
