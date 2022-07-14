f6d513eb37aa5ac7d625bb8d0485bf0a52ed3463
«Viktoriya <v.oberemko@mail.ru>
2022-07-14 11:38:50 +0300

    Add SolutionTask7_HomeWork3

---
+++ SolutionTask7_HW3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число.");
+string? inputLkneOne = Console.ReadLine();
+
+if (inputLkneOne != null )
+{
+    int inputNumberOne = int.Parse(inputLkneOne);
+      
+    if (inputNumberOne % 2 == 0)
+    Console.WriteLine("Число четное");
+    
+    else
+    Console.WriteLine("Число нечетное");
+}
+    
+
+
+
+
