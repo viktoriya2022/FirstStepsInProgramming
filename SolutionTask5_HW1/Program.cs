371218998085c51d7864b8f7d08326092d2f8e28
«Viktoriya <v.oberemko@mail.ru>
2022-07-14 11:37:38 +0300

    Add SolutionTask5_HomeWork1

---
+++ SolutionTask5_HW1/Program.cs
@@ -0,0 +1,16 @@
+﻿string? inputLkneOne = Console.ReadLine();
+string? inputLkneTwo = Console.ReadLine();
+
+if(inputLkneOne != null && inputLkneTwo !=null)
+{
+  int inputNumberOne = int.Parse(inputLkneOne);
+  int inputNumberTwo = int.Parse(inputLkneTwo);
+ if(inputNumberOne > inputNumberTwo)
+       {
+           Console.WriteLine(inputNumberOne);
+       }
+ else
+       {
+           Console.WriteLine(inputNumberTwo);
+       }
+}
