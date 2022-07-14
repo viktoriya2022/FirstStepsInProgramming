ff33c5d3721e6af2ef28effd86d695c3ca150d17
«Viktoriya <v.oberemko@mail.ru>
2022-07-14 11:38:21 +0300

    Add SolutionTask6_HomeWork2

---
+++ SolutionTask6_HW2/Program.cs
@@ -0,0 +1,30 @@
+﻿string? inputLkneOne = Console.ReadLine();
+string? inputLkneTwo = Console.ReadLine();
+string? inputLkneThree = Console.ReadLine();
+
+if (inputLkneOne != null && inputLkneTwo != null && inputLkneThree != null)
+{
+    int inputNumberOne = int.Parse(inputLkneOne);
+    int inputNumberTwo = int.Parse(inputLkneTwo);
+    int inputNumberThree = int.Parse(inputLkneThree);
+
+
+    if (inputNumberOne > inputNumberTwo)
+    {
+        if (inputNumberOne > inputNumberThree)
+            Console.WriteLine(inputNumberOne);
+
+        else
+            Console.WriteLine(inputNumberThree);
+    }
+    else
+    {
+        if (inputNumberTwo > inputNumberThree)
+            Console.WriteLine(inputNumberTwo);
+
+        else
+            Console.WriteLine(inputNumberThree);
+    }
+}
+
+
