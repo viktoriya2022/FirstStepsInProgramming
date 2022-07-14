da4dbd941f772bc1d54ec9ab583ad31238530b5c
«Viktoriya <v.oberemko@mail.ru>
2022-07-14 13:32:18 +0300

    Add second solution for task 8

--- SolutionTask8_HW4/Program.cs
+++ SolutionTask8_HW4/Program.cs
@@ -1,15 +1,32 @@
-﻿string? inputLkneOne = Console.ReadLine();
+﻿// string? inputLkneOne = Console.ReadLine();
+
+// if(inputLkneOne != null)
+// {
+//    int inputNumber = int.Parse(inputLkneOne);
+//    int startNumber =  2;
+//    string lineOutput = "";
+//    while (startNumber < inputNumber)
+//    {
+//     lineOutput = lineOutput + startNumber +", ";
+//     startNumber = startNumber + 2;
+//    }
+    
+//    Console.WriteLine(lineOutput);
+// }
+
+
+string? inputLkneOne = Console.ReadLine();
 
 if(inputLkneOne != null)
 {
    int inputNumber = int.Parse(inputLkneOne);
    int startNumber =  2;
    string lineOutput = "";
-   while (startNumber < inputNumber)
+   while (startNumber <= inputNumber - 2)
    {
     lineOutput = lineOutput + startNumber +", ";
     startNumber = startNumber + 2;
    }
-    
+   lineOutput = lineOutput + startNumber;
    Console.WriteLine(lineOutput);
 }
