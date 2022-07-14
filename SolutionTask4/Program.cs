eac94dc519bc957c5f0303fb7c68ef5332b59f1e
«Viktoriya <v.oberemko@mail.ru>
2022-07-14 11:36:23 +0300

    Add SolutionTask4

---
+++ SolutionTask4/Program.cs
@@ -0,0 +1,32 @@
+﻿string? inputLine = Console.ReadLine();
+
+if(inputLine != null)
+{
+   int inputNumber = int.Parse(inputLine);
+
+   int outputNumber = inputNumber%10;
+   
+   Console.WriteLine(outputNumber);
+}
+
+
+
+// string? inputLkneOne = Console.ReadLine();
+
+// if(inputLkneOne != null)
+// {
+//     string lastNum = inputLkneOne.Substring(2,1);
+//     Console.WriteLine(lastNum);
+// }
+
+
+
+
+// string? inputLine = Console.ReadLine();
+
+// if(inputLine != null)
+// {
+//     char[] M = inputLine.ToCharArray();
+//     Console.WriteLine(M[2]);
+// }
+
