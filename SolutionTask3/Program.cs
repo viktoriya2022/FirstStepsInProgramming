2ecab547ccb4f678cbf62ea02294471212d39d15
«Viktoriya <v.oberemko@mail.ru>
2022-07-14 11:35:53 +0300

    Add SolutionTask3

---
+++ SolutionTask3/Program.cs
@@ -0,0 +1,30 @@
+﻿// string? inputLkneOne = Console.ReadLine();
+
+// if(inputLkneOne != null)
+// {
+//    int inputNumber = int.Parse(inputLkneOne);
+//    int startNumber =  (inputNumber * -1);
+//    string lineOutput = "";
+//    while (startNumber < inputNumber)
+//    {
+//     lineOutput = lineOutput + startNumber +", ";
+//     startNumber++;
+//    }
+//     lineOutput = lineOutput + inputNumber;
+//    Console.WriteLine(lineOutput);
+// }
+string? inputLkneOne = Console.ReadLine();
+
+if(inputLkneOne != null)
+{
+   int inputNumber = int.Parse(inputLkneOne);
+   int startNumber =  1;
+   string lineOutput = "0";
+   while (startNumber <= inputNumber)
+   {
+    lineOutput = startNumber*(-1)+", " +lineOutput+", "+ startNumber;
+    startNumber++;
+   }
+  
+   Console.WriteLine(lineOutput);
+}
