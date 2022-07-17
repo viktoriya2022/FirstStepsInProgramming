61a286ec6d31a8ef1913cbe300bf28390c16137c
«Viktoriya <v.oberemko@mail.ru>
2022-07-17 11:08:10 +0300

    Add SolutionTask15_HW3

---
+++ SolutionTask15_HW3/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите число от одного до семи.");
+
+int inputLkneOne = int.Parse(Console.ReadLine());
+
+if (inputLkneOne != null)
+{
+    if (inputLkneOne == 6 || inputLkneOne == 7)
+    {
+        Console.WriteLine("Ура! Выходной!");
+    }
+    else
+    {
+        if (inputLkneOne > 7)
+        {
+            Console.WriteLine("Введено число больше семи.");
+        }
+        else
+        {
+            Console.WriteLine("Это рабочий день.");
+        }
+    }
+}
+
