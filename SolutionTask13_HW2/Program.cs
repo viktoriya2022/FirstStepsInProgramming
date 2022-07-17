ba79a8898aaff30a8704d09aa89d03feadce5228
«Viktoriya <v.oberemko@mail.ru>
2022-07-17 10:48:35 +0300

    Add SolutionTask13_HW2

---
+++ SolutionTask13_HW2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Enter number");
+//Считываем строковую переменную
+string? inputLkneOne = Console.ReadLine(); 
+//преобразуем в массив
+char[] inputLkneTwo = inputLkneOne.ToCharArray(); 
+
+if (inputLkneOne != null)
+{
+    if (inputLkneTwo.Length >= 3)
+    {
+        //выводим третью цифру
+        Console.WriteLine("the third digit is " + inputLkneTwo[2]); 
+    }
+    else
+    {
+        Console.WriteLine("No third digit");
+    }
+}
