1b35d5629bfb3fb19d1cb44682b0d26d3fca5169
«Viktoriya <v.oberemko@mail.ru>
2022-07-17 10:14:59 +0300

    Add SolutionTask10_HW1

---
+++ SolutionTask10_HW1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите трехзначное число");
+
+string? inputLkneOne = Console.ReadLine();
+
+if (inputLkneOne != null)
+{
+    int inputNumberOne = int.Parse(inputLkneOne);
+
+    int secondNumber = (inputNumberOne % 100) / 10;
+    Console.WriteLine(secondNumber);
+}
--- SolutionTask12/Program.cs
+++ SolutionTask12/Program.cs
@@ -2,7 +2,8 @@
 // string? inputLkneOne = Console.ReadLine();
 // string? inputLkneTwo = Console.ReadLine();
 
-// if(inputLkneOne != null && inputLkneTwo != null){
+// if(inputLkneOne != null && inputLkneTwo != null)
+//{
 // int inputNumberOne = int.Parse(inputLkneOne);
 // int inputNumberTwo = int.Parse(inputLkneTwo);
 
