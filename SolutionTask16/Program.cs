fc14e49c74e6ac003266928ad015ac8910019ade
«Viktoriya <v.oberemko@mail.ru>
2022-07-17 09:44:09 +0300

    Add SolutionTask16

---
+++ SolutionTask16/Program.cs
@@ -0,0 +1,24 @@
+﻿//Никина
+Console.Clear();
+
+Console.WriteLine("Введите первое число:");
+string? inputFirst = Console.ReadLine();
+Console.WriteLine("Введите второе число:");
+string? inputSecond = Console.ReadLine();
+
+if ((inputFirst != null) && (inputSecond != null))
+{
+int inputNumberFirst = int.Parse(inputFirst);
+int inputNumberSecond = int.Parse(inputSecond);
+int a = inputNumberFirst * inputNumberFirst;
+int b = inputNumberSecond * inputNumberSecond;
+if ((a == inputNumberSecond) || (b == inputNumberFirst))
+{
+Console.WriteLine("Да!");
+}
+else
+{
+Console.WriteLine("Нет!");
+}
+
+}
