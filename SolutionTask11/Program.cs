6d2eac9850e02f1ccf449cd35d66fa9d1a57ecec
«Viktoriya <v.oberemko@mail.ru>
2022-07-17 08:58:53 +0300

    Add  SolutionTask11

---
+++ SolutionTask11/Program.cs
@@ -0,0 +1,35 @@
+﻿// // Артем Малистов
+// int number = new Random().Next(100,1000);
+// Console.WriteLine("Число:" + number);
+// int numberSecondDigit = number/100;
+// int numberThirdDigit = number%10;
+// Console.WriteLine("Число после удаления второй цифры: " + numberSecondDigit + numberThirdDigit);
+// //но число не собрано
+
+// Kirill Novikov: 
+void middleDigitCutter()
+{
+int number = new Random().Next(100, 1000);
+Console.Write("The random number is : ");
+Console.WriteLine(number);
+string stringNum = number.ToString();
+Console.Write("The modified num is : ");
+Console.Write(stringNum[0]);
+Console.WriteLine(stringNum[2]);
+}
+//число не собрано
+void middleDigitCutter2()
+{
+int number = new Random().Next(100, 1000);
+Console.Write("The random number is : ");
+Console.WriteLine(number);
+
+int num1 = number / 100;
+int num2 = number % 10;
+
+Console.Write("The modified num is : ");
+Console.Write(num1*10 + num2);
+}
+// в этом методе число собрано верно
+middleDigitCutter();
+middleDigitCutter2();
