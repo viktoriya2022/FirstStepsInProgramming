88593eb5c6d6f574c6c2d1a531e86ee8a19be838
«Viktoriya <v.oberemko@mail.ru>
2022-07-18 17:42:16 +0300

    Add SolutionTask18

---
+++ SolutionTask18/Program.cs
@@ -0,0 +1,22 @@
+﻿void printAnswer(int number)
+{
+    if (number == 1) Console.WriteLine("Допустимо: x>0, y>0");
+    if (number == 2) Console.WriteLine("Допустимо: x<0, y>0");
+    if (number == 3) Console.WriteLine("Допустимо: x<0, y<0");
+    if (number == 4) Console.WriteLine("Допустимо: x>0, y<0");
+
+}
+
+//считает строчку с консоли
+string? inputLine = Console.ReadLine();
+
+//проверит строчку (не пустая?)
+if (inputLine != null)
+{
+    //превратит строчку в вещественное число
+    int inputNumber = int.Parse(inputLine);
+    //передаст число в метод (наверх)
+    printAnswer(inputNumber);
+}
+
+
