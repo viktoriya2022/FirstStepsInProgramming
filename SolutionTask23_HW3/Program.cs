d9d352f4e4ad10bdd039b5823811b43630c864fb
«Viktoriya <v.oberemko@mail.ru>
2022-07-20 14:58:46 +0300

    Add SolutionTask23_HomeWork3

---
+++ SolutionTask23_HW3/Program.cs
@@ -0,0 +1,21 @@
+﻿//Считываем переменную
+string? inputLineN = Console.ReadLine();
+
+if (inputLineN != null) // цикл проверки
+{
+    int numberN = int.Parse(inputLineN);
+    //объявляем переменные (lineN и lineNN пустые строки)
+    string lineN = "";
+    string lineCubeN = string.Empty;
+    int s = 1;
+    while (s <= numberN)
+    {
+        lineN = lineN + s + " ";
+        lineCubeN = lineCubeN + Math.Pow(s, 3) + " ";
+        s++;
+    }
+
+    Console.WriteLine(lineN);
+    Console.WriteLine(lineCubeN);
+}
+
