5bf7cd5d9755d0625fd0442854aae684483f3d58
«Viktoriya <v.oberemko@mail.ru>
2022-07-21 17:40:22 +0300

    Add SolutionTask23_HomeWork3 (вывод на консоль таблицы)

--- SolutionTask23_HW3/Program.cs
+++ SolutionTask23_HW3/Program.cs
@@ -1,4 +1,29 @@
-﻿//Считываем переменную
+﻿// Console.WriteLine("Введите число для возведения в куб:");
+// //Считываем переменную
+// string? inputLineN = Console.ReadLine();
+
+// if (inputLineN != null) // цикл проверки
+// {
+//     int numberN = int.Parse(inputLineN);
+//     //объявляем переменные (lineN и lineNN пустые строки)
+//     string lineN = "";
+//     string lineCubeN = string.Empty;
+//     int s = 1;
+//     while (s <= numberN)
+//     {
+//         lineN = lineN + s + " ";
+//         lineCubeN = lineCubeN + Math.Pow(s, 3) + " ";
+//         s++;
+//     }
+
+//     Console.WriteLine(lineN);
+//     Console.WriteLine(lineCubeN);
+
+//--------------------------------------------------------------------------------------------------
+// Решение с выводом в таблицу
+
+Console.WriteLine("Введите число для возведения в куб:");
+//Считываем переменную
 string? inputLineN = Console.ReadLine();
 
 if (inputLineN != null) // цикл проверки
@@ -12,10 +37,9 @@ if (inputLineN != null) // цикл проверки
     {
         lineN = lineN + s + " ";
         lineCubeN = lineCubeN + Math.Pow(s, 3) + " ";
+        Console.WriteLine(new string('-', 23));
+        Console.WriteLine("|{0,7}   |{1,10}|", s, Math.Pow(s, 3));
         s++;
     }
-
-    Console.WriteLine(lineN);
-    Console.WriteLine(lineCubeN);
+    Console.WriteLine(new string('-', 23));
 }
-
