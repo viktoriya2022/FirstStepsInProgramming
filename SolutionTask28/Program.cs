e9ea90372e52fb5260f37181d93c24e006710b9a
«Viktoriya <v.oberemko@mail.ru>
2022-08-04 16:40:05 +0300

    Add SolutionTask28

---
+++ SolutionTask28/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число N и выдает произведение чисел от 1 до N.
+
+Console.Write("Введите число: ");
+string? inputLine = Console.ReadLine();
+int inputNum = int.Parse(inputLine);
+
+Console.WriteLine(sumNums(inputNum));
+
+int sumNums(int num)
+{
+    int sum = 1;
+    for (int i = 1; i <= inputNum; i++)
+    {
+        //sum *= i;
+        sum = sum * i;
+    }
+    return sum;
+}
