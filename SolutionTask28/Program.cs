94a497a273de1550f86d02f51aaf8b209a316dc4
«Viktoriya <v.oberemko@mail.ru>
2022-08-04 20:46:07 +0300

    Add SolutionTask28

--- SolutionTask28/Program.cs
+++ SolutionTask28/Program.cs
@@ -1,18 +1,38 @@
 ﻿// Напишите программу, которая принимает на вход число N и выдает произведение чисел от 1 до N.
 
+// Решение Кирилла (рекурентное)
+
 Console.Write("Введите число: ");
 string? inputLine = Console.ReadLine();
 int inputNum = int.Parse(inputLine);
+int t;
 
+t = Environment.TickCount;
 Console.WriteLine(sumNums(inputNum));
+Console.WriteLine("Simple time: {0} ms", Environment.TickCount - t);
+
+t = Environment.TickCount;
+Console.WriteLine(mulRec(inputNum));
+Console.WriteLine("Recurs time: {0} ms", Environment.TickCount - t);
 
 int sumNums(int num)
 {
     int sum = 1;
     for (int i = 1; i <= inputNum; i++)
     {
-        //sum *= i;
-        sum = sum * i;
+        sum *= i;
     }
     return sum;
+}
+// рекурсия
+int mulRec(int num)
+{
+    if (num == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return num * mulRec(num - 1);
+    }
 }
