a1983e28bf9d07ecf86a53b44d035418229602e1
«Viktoriya <v.oberemko@mail.ru>
2022-08-06 19:02:00 +0300

    Add SolutionTask27_HomeWork 2

---
+++ SolutionTask27/Program.cs
@@ -0,0 +1,28 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+//Очищаем консоль:
+Console.Clear();
+
+//Выводим сообщение для пользователя:
+Console.Write("Введите число: ");
+
+string inputLineNumber = Console.ReadLine() ?? ""; //Вместо проверки на null
+int inputNumber = int.Parse(inputLineNumber);
+int result = 0;
+
+//Вводим метод расчета суммы цифр в числе:
+void SumDigOfNumber()
+{
+while (inputNumber > 10)
+{
+    result = result + inputNumber % 10;
+    inputNumber = inputNumber / 10;
+}
+result=result+inputNumber;
+}
+
+//Вызываем метод расчета:
+SumDigOfNumber();
+
+//Выводим результат на консоль:
+Console.WriteLine("Сумма цифр числа "+inputLineNumber+" равна "+result+".");
