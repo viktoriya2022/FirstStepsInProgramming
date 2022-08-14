5983d39320d2be796755c62a359c78ebb16a45e0
«Viktoriya <v.oberemko@mail.ru>
2022-08-14 13:56:41 +0300

    Add SolutionTask41_HomeWork 1

---
+++ SolutionTask41_HW1/Program.cs
@@ -0,0 +1,72 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//====================================
+
+Console.Clear();
+
+Console.Write("Введите количество элементов массива: ");
+int quantity = int.Parse(Console.ReadLine());
+Console.WriteLine("Вводите элементы массива: ");
+
+//Объявляем новый массив
+int[] array = new int[quantity];
+
+//Метод заполнения массива с клавиатуры
+int[] FillingArray(int[] array, int quantity)
+{
+    int elementOfArray = 0;
+    while (elementOfArray < quantity)
+    {
+        array[elementOfArray] = int.Parse(Console.ReadLine());
+        elementOfArray++;
+    }
+    return array;
+}
+
+//Метод печати массива
+void PrintIntArray(int[] inputArray)
+{
+    //Буферная переменная
+    int i = 0;
+    //Пробегаем все элементы массива
+    while (i < inputArray.Length - 1)
+    {
+        //Выводим элемент массива
+        Console.Write(inputArray[i] + ", ");
+        //Увеличиваем инкремент
+        i++;
+
+    }
+    Console.WriteLine(inputArray[i]);
+}
+
+//Метод подсчета количества положительных элементов в массиве
+int Calculate(int[] array)
+{
+    int count = 0;
+    int i = 0;
+    while (i < array.Length)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+            i++;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    return count;
+}
+
+//Метод печати результата подсчета
+void PrintResult(int count)
+{
+    Console.WriteLine("Количество введенных положительных чисел: " + count);
+}
+
+FillingArray(array, quantity);
+PrintIntArray(array);
+PrintResult(Calculate(array));
+
+
---
+++ SolutionTask45/Program.cs
@@ -0,0 +1,70 @@
+﻿//  Напишите программу, которая будет создавть копию заданного одномерного массива
+//  с помощью поэлементного копирования.
+//=============================================
+
+Console.Clear();
+
+int[] FillingArray()
+{
+    //Выходной массив
+    int[] outArray = new int[123];
+    //Буферная переменная
+    int i = 0;
+    //Создаем экземпляр класса рандомайзер
+    System.Random numberSintezator = new System.Random();
+    //Цикл заполнения массива
+    while (i < 123)
+    {
+        //Получаем новое значение (произвольные, т.е Next не определен-пустые скобки)
+        outArray[i] = numberSintezator.Next(-1000, 1000);
+        //Увеличиваем инкремент
+        i++;
+
+    }
+    //Возвращаем значение
+    return outArray;
+}
+
+//Метод печатает массив
+void PrintIntArray(int[] inputArray)
+{
+    //Буферная переменная
+    int i = 0;
+    //Пробегаем все элементы массива
+    while (i < inputArray.Length - 1)
+    {
+        //Выводим элемент массива
+        Console.Write(inputArray[i] + ", ");
+        //Увеличиваем инкремент
+        i++;
+
+    }
+    Console.WriteLine(inputArray[i]);
+}
+
+
+//тип object позволяет чделать метод универлальным, без необходимости указывать тип данных
+object[] CopyArrayStandartTool(params object[] inputArray)
+{
+    object[] buferArray=new object[inputArray.Length];
+    inputArray.CopyTo(buferArray,0);
+    return buferArray;
+}
+int[] testArray=FillingArray();
+PrintIntArray(testArray);
+int[] outArray=(int[])CopyArrayStandartTool(testArray);
+PrintIntArray(outArray);
+
+// string[] CopyArrayStandartTool(int[] inputArray)
+// {
+//     string[] buferArray=new int[nnputArray.Length];
+//     inputArray.CopyTo(buferArray,0);
+//     return buferArray;
+// }
+
+// double[] CopyArrayStandartTool(int[] inputArray)
+// {
+//     double[] buferArray=new int[nnputArray.Length];
+//     inputArray.CopyTo(buferArray,0);
+//     return buferArray;
+// }
