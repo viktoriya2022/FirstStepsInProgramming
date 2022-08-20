80b6244bca13a0fecaafe3421f4f01129d1c7e00
«Viktoriya <v.oberemko@mail.ru>
2022-08-20 18:47:36 +0300

    Add SolutionTask 54,56,58,60,62(variant from Ira)

--- SolutionTask43_HW2/Program.cs
+++ SolutionTask43_HW2/Program.cs
@@ -4,35 +4,46 @@
 
 Console.Clear();
 
-//Объявляем переменные:
-double k1;
-double k2;
-double b1;
-double b2;
+//Объявляем переменные. одновременно вызываем метод считывания данных с использованием строковой переменной
+double k1 = ReadCoord("Введите значение k1: ");
+double k2 = ReadCoord("Введите значение k2: ");
+double b1 = ReadCoord("Введите значение b1: ");
+double b2 = ReadCoord("Введите значение b2: ");
 double x;
 double y;
 
-Read();
+//Read();
 Calculate();
 PrintResult();
 
-//Метод для считывания введенных координат:
-void Read()
+// //Метод для считывания введенных координат:
+// void Read()
+// {
+//     //Выводим на консоль сообщение для пользователя
+//     Console.WriteLine("Введите значение b1: ");
+//     //Распарсиваем введенную переменную в новую
+//     b1 = double.Parse(Console.ReadLine());
+
+//     Console.WriteLine("Введите значение k1: ");
+//     k1 = double.Parse(Console.ReadLine());
+
+//     Console.WriteLine("Введите значение b2: ");
+//     b2 = double.Parse(Console.ReadLine());
+
+//     Console.WriteLine("Введите значение k2: ");
+//     k2 = double.Parse(Console.ReadLine());
+
+// }
+
+// метод для считывания введенных пользователем данных с консоли
+// метод получает координаты от пользователя
+//возвращает введенные значения в нужном формате
+double ReadCoord(string line)
 {
-    //Выводим на консоль сообщение для пользователя
-    Console.WriteLine("Введите значение b1: ");
-    //Распарсиваем введенную переменную в новую
-    b1 = double.Parse(Console.ReadLine());
+    Console.Write(line);
+    double buf = double.Parse(Console.ReadLine());
 
-    Console.WriteLine("Введите значение k1: ");
-    k1 = double.Parse(Console.ReadLine());
-
-    Console.WriteLine("Введите значение b2: ");
-    b2 = double.Parse(Console.ReadLine());
-
-    Console.WriteLine("Введите значение k2: ");
-    k2 = double.Parse(Console.ReadLine());
-
+    return buf;
 }
 
 //Метод расчета координат точки пересечения двух прямых:
