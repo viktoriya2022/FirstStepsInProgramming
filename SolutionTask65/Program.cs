5b27f34d8a1be94c11f4d49ac81b0c4d943b1ca1
«Viktoriya <v.oberemko@mail.ru>
2022-08-26 08:10:38 +0300

    Add SolutionTask65

---
+++ SolutionTask65/Program.cs
@@ -0,0 +1,25 @@
+﻿// =====================================================================================================
+// Задайте значения M и N. Напишите программу, которая выведет натуральные числа в промежутке от M до N.
+// =====================================================================================================
+
+Console.Clear();
+
+int numberM = ReadData("Задайте нижнюю границу числового поля: ");
+int numberN = ReadData("Задайте верхнюю границу числового поля: ");
+
+if (numberM < numberN) NaturalNumberPrinter(numberM, numberN);
+else NaturalNumberPrinter(numberN, numberM);
+
+//метод считывает с кoнсоли
+int ReadData(string line)
+{
+    Console.WriteLine(line);
+    return int.Parse(Console.ReadLine());
+}
+void NaturalNumberPrinter(int m, int n)
+{
+    if (m - 1 == n) return; //stop
+    Console.Write(m + " ");
+    NaturalNumberPrinter(m + 1, n);
+}
+
